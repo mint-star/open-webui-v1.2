40c48af5c291da552b9c86ec929bf8c5c965d284
jignasu-disney <JignasuSolanki.TPA@jiostar.com>
2026-02-23 12:40:12 +0530

    removed static email

--- src/lib/components/TitanSupport.svelte
+++ src/lib/components/TitanSupport.svelte
@@ -317,7 +317,7 @@
       const formData = new FormData();
       formData.append("request_type", requestTypeMap[requestType]);
       formData.append("description", description.trim());
-      formData.append("email", "jignasusolanki.tpa@jiostar.com");
+      formData.append("email", $user.email || "");
 
       uploadedFiles.forEach((fileObj, index) => {
         formData.append(`attachments_attributes[${index}][attachment_file]`, fileObj.file);
